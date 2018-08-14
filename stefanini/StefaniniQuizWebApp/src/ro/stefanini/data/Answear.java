890fadc54f0db7013be246685868776b39fec139
George <palasanug@gmail.com>
2018-08-14 05:29:06 +0300

    Facut clasele de Answear si Question

--- stefanini/StefaniniQuizWebApp/src/ro/stefanini/data/Answear.java
+++ stefanini/StefaniniQuizWebApp/src/ro/stefanini/data/Answear.java
@@ -1,5 +1,19 @@
 package ro.stefanini.data;
 
 public class Answear {
-
+	private String contentAnswear;
+	private Boolean truthValue;
+	
+	public String getAnswear() {
+		return contentAnswear;
+	}
+	public Boolean getValue() {
+		return truthValue;
+	}
+	public void setAnswear(String contentAnswear) {
+		this.contentAnswear = contentAnswear;
+	}
+	public void setValue(Boolean value) {
+		this.truthValue = value;
+	}
 }
--- stefanini/StefaniniQuizWebApp/src/ro/stefanini/data/Question.java
+++ stefanini/StefaniniQuizWebApp/src/ro/stefanini/data/Question.java
@@ -1,5 +1,38 @@
 package ro.stefanini.data;
+import java.util.*;
 
 public class Question {
-
+	private String questionContent;
+	List<Answear> answears = new ArrayList<Answear>();
+	
+	public Question(String questionContent, Answear[] answears) {
+		this.questionContent = questionContent;
+		 for (int i = 0; i < answears.length; i++) {
+			 this.answears.add(answears[i]);
+	        }	
+	}
+	
+	public void setQuestion(String questionContent) {
+		this.questionContent = questionContent;
+	}
+	
+	public void addAnswears(Answear[] answears) {
+		 for (int i = 0; i < answears.length; i++) {
+			 this.answears.add(answears[i]);
+	        }
+	}
+	public void addAnswear(Answear answear) {
+		this.answears.add(answear);
+	}
+	
+	public String getQuestion() {
+		return this.questionContent;
+	}
+	
+	public Answear[] getAnswears() {
+		Answear[] answears = new Answear[this.answears.size()];
+		answears = this.answears.toArray(answears);
+		return answears;
+	}
+	
 }
